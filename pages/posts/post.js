54cdbe0cb812c41dbbc0ed6b3337306f1af6e076
hoggenw <253192463@qq.com>
2018-08-24 10:22:51 +0800

    no message

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -4,10 +4,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
   },
   imagepath:"/images/",
   process:function() {
+    var date = "Nov 17 2018";
 
   },
   /**
@@ -15,6 +15,55 @@ Page({
    */
   onLoad: function (options) {
     console.log("onLoad");
+    var posts_contents = [
+      {
+        date: "Nov 17 2018",
+        title: "达到减肥的",
+        img: {
+          post_img: "/images/post/crab.png",
+          author_img: "/images/avatar/4.png",
+        },
+        img_condition: true,
+        a: "1",
+        b: 2,
+        c: 3,
+        content: "第二步，打开微信开发者工具，选择新建小程序项目，我们先不需理解AppID的概念，新建项目时选择无AppID，并取消勾选“建立普通快速启动模板”的选项。",
+        view_num: "1120",
+        conllect_num: "98",
+      },
+      {
+        date: "Nov 17 2018",
+        title: "达到减sds肥的",
+        img: {
+          post_img: "/images/post/vr.png",
+          author_img: "/images/avatar/5.png",
+        },
+        img_condition: true,
+        a: "1",
+        b: 2,
+        c: 3,
+        content: "组件模版的写法与页面模板相同。组件模版与组件数据结合后生成的节点树，将被插入到组件的引用位置上。在组件模板中可以提供一个<slot> 节点，用于承载组件引用时提供的子节点。",
+        view_num: "3120",
+        conllect_num: "298",
+      },
+      {
+        date: "Feb 27 2018",
+        title: "都是块级的",
+        img: {
+          post_img: "/images/post/bl.png",
+          author_img: "/images/avatar/2.png",
+        },
+        img_condition: true,
+        a: "1",
+        b: 2,
+        c: 3,
+        content: " 一个服务仅仅只有界面展示是不够的，还需要和用户做交互：响应用户的点击、获取用户的位置等等。在小程序里边，我们就通过编写 JS 脚本文件来处理用户的操作。",
+        view_num: "2340",
+        conllect_num: "198",
+      }
+    ]
+
+      this.setData({post_key:posts_contents});
   },
 
   /**
