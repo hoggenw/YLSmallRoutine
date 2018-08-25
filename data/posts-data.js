0d773f21137229a47d6452a727efa461d3829afd
hoggenw <253192463@qq.com>
2018-08-25 13:23:12 +0800

    no message

---
+++ data/posts-data.js
@@ -0,0 +1,86 @@
+var local_database = [
+  {
+    date: "Nov 17 2018",
+    title: "达到减肥的",
+    img: {
+      post_img: "/images/post/crab.png",
+      author_img: "/images/avatar/4.png",
+    },
+    img_condition: true,
+    content: "第二步，打开微信开发者工具，选择新建小程序项目，我们先不需理解AppID的概念，新建项目时选择无AppID，并取消勾选“建立普通快速启动模板”的选项。",
+    view_num: "1120",
+    conllect_num: "98",
+    postId:0,
+  },
+  {
+    date: "Nov 17 2018",
+    title: "达到减sds肥的",
+    img: {
+      post_img: "/images/post/vr.png",
+      author_img: "/images/avatar/5.png",
+    },
+    img_condition: true,
+    content: "组件模版的写法与页面模板相同。组件模版与组件数据结合后生成的节点树，将被插入到组件的引用位置上。在组件模板中可以提供一个<slot> 节点，用于承载组件引用时提供的子节点。",
+    view_num: "3120",
+    conllect_num: "298",
+    postId: 1,
+  },
+  {
+    date: "Feb 27 2018",
+    title: "都是块级的",
+    img: {
+      post_img: "/images/post/bl.png",
+      author_img: "/images/avatar/2.png",
+    },
+    img_condition: true,
+    content: " 一个服务仅仅只有界面展示是不够的，还需要和用户做交互：响应用户的点击、获取用户的位置等等。在小程序里边，我们就通过编写 JS 脚本文件来处理用户的操作。",
+    view_num: "2340",
+    conllect_num: "198",
+    postId: 2,
+  },
+  {
+    date: "Nov 17 2018",
+    title: "达到减肥的",
+    img: {
+      post_img: "/images/post/crab.png",
+      author_img: "/images/avatar/4.png",
+    },
+    img_condition: true,
+    content: "第二步，打开微信开发者工具，选择新建小程序项目，我们先不需理解AppID的概念，新建项目时选择无AppID，并取消勾选“建立普通快速启动模板”的选项。",
+    view_num: "1120",
+    conllect_num: "98",
+    postId: 3,
+  },
+  {
+    date: "Nov 17 2018",
+    title: "达到减sds肥的",
+    img: {
+      post_img: "/images/post/vr.png",
+      author_img: "/images/avatar/5.png",
+    },
+    img_condition: true,
+    content: "组件模版的写法与页面模板相同。组件模版与组件数据结合后生成的节点树，将被插入到组件的引用位置上。在组件模板中可以提供一个<slot> 节点，用于承载组件引用时提供的子节点。",
+    view_num: "3120",
+    conllect_num: "298",
+    postId: 4,
+  },
+  {
+    date: "Feb 27 2018",
+    title: "都是块级的",
+    img: {
+      post_img: "/images/post/bl.png",
+      author_img: "/images/avatar/2.png",
+    },
+    img_condition: true,
+    content: " 一个服务仅仅只有界面展示是不够的，还需要和用户做交互：响应用户的点击、获取用户的位置等等。在小程序里边，我们就通过编写 JS 脚本文件来处理用户的操作。",
+    view_num: "2340",
+    conllect_num: "198",
+    postId: 5,
+  },
+]
+
+var a = "2"
+module.exports = {
+  postList: local_database,
+  a_key:a
+}
