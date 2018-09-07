78a42a3fe774817a97da1c98710ae4cc511e6e60
hoggenw <253192463@qq.com>
2018-09-07 16:11:29 +0800

    finish

---
+++ YlReaderAndMovie/pages/movies/movies-detail/movies-detail.js
@@ -0,0 +1,139 @@
+// pages/movies/movies-detail/movies-detail.js
+var util = require('../../../utils/utils.js')
+var app = getApp();
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    movie: {}
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    var movieId = options.movieId;
+    var url = "http://api.douban.com/v2/movie/subject/" + movieId + "?apikey=0df993c66c0c636e29ecbb5344252a4a";
+    this.requestDataWithUrl(url, 'kong');
+    console.log(movieId);
+  },
+
+
+  requestDataWithUrl: function(ulrString, key) {
+    var that = this;
+    wx.request({
+
+      url: ulrString,
+
+      //必须要设置"content-type":"json",不然会报错 400 (Bad Request)
+      method: 'GET',
+      header: {
+
+        "content-type": "json"
+
+      },
+
+      success: function(backData) {
+
+        that.processDoubanData(backData.data, key);
+        
+      }
+
+    })
+
+  },
+
+  processDoubanData:function(data,key){
+    //console.log(data);
+    if (!data) {
+      return;
+    }
+    var director = {
+      avatar: "",
+      name: "",
+      id: ""
+    }
+    if (data.directors[0] != null) {
+      if (data.directors[0].avatars != null) {
+        director.avatar = data.directors[0].avatars.large
+
+      }
+      director.name = data.directors[0].name;
+      director.id = data.directors[0].id;
+    }
+    var movie = {
+      movieImg: data.images ? data.images.large : "",
+      country: data.countries[0],
+      title: data.title,
+      originalTitle: data.original_title,
+      wishCount: data.wish_count,
+      commentCount: data.comments_count,
+      year: data.year,
+      generes: data.genres.join("、"),
+      stars: util.convertToStarsArray(data.rating.stars),
+      score: data.rating.average,
+      director: director,
+      casts: util.convertToCastString(data.casts),
+      castsInfo: util.convertToCastInfos(data.casts),
+      summary: data.summary
+    }
+    console.log(movie);
+    this.setData({
+      movie:movie
+    })
+
+  }
+
+
+
+  // /**
+  //  * 生命周期函数--监听页面初次渲染完成
+  //  */
+  // onReady: function () {
+
+  // },
+
+  // /**
+  //  * 生命周期函数--监听页面显示
+  //  */
+  // onShow: function () {
+
+  // },
+
+  // /**
+  //  * 生命周期函数--监听页面隐藏
+  //  */
+  // onHide: function () {
+
+  // },
+
+  // /**
+  //  * 生命周期函数--监听页面卸载
+  //  */
+  // onUnload: function () {
+
+  // },
+
+  // /**
+  //  * 页面相关事件处理函数--监听用户下拉动作
+  //  */
+  // onPullDownRefresh: function () {
+
+  // },
+
+  // /**
+  //  * 页面上拉触底事件的处理函数
+  //  */
+  // onReachBottom: function () {
+
+  // },
+
+  // /**
+  //  * 用户点击右上角分享
+  //  */
+  // onShareAppMessage: function () {
+
+  // }
+})
